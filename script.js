10572b87b8059f2192fa1227b89dc6b68e48742c
maureen <maureenotieno38@gmail.com>
2024-04-24 16:14:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const notesContainer=document.querySelector(".notes-container");
+const createBtn=document.querySelector(".btn");
+let notes=document.querySelectorAll("input-box");
+
+
+function showNotes(){
+    notesContainer.innerHTML=localStorage.getItem("notes")||"";
+}
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes",notesContainer.innerHTML);
+}
+
+createBtn.addEventListener("click",()=>{
+    let inputBox=document.createElement("p");
+    let img=document.createElement("img");
+    inputBox.setAttribute("contenteditable","true");
+    inputBox.className="input-box";
+    img.src="images/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+    updateStorage();
+});
+notesContainer.addEventListener("click",function(e){
+    if(e.target.tagName=="IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    }
+})
